d8c5ae09c401a5c57afb7da3d65735a02fbbdd69
irmagine <60980371+irmagine@users.noreply.github.com>
2020-02-14 06:02:31 +0100

    Add files via upload

--- offerte.py
+++ offerte.py
@@ -17,6 +17,7 @@ with open("offerten.csv", "r") as file:
 
 print(gap)
 
+
 steuer_mon = []
 
 with open("steuer.csv", "r") as file:
@@ -28,7 +29,6 @@ with open("steuer.csv", "r") as file:
         steuer_mon.append(steuer_betr)
         steuer_jahr = sum(steuer_mon)
 print(steuer_mon)
-# print(steuer_jahr)
 
 cf_month = []
 
@@ -41,7 +41,6 @@ with open("Cashflow.csv", "r") as file:
         cf_month.append(cf)
 
 
-
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
@@ -51,6 +50,7 @@ ax.plot(month, steuer_mon, label="Acc for taxes")
 ax.legend()
 
 plt.show()
+
 
 
 
